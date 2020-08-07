6737aa5da36ac10248d2a16a24ab44c680612922
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-07 10:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code here
+
+function createEmployeeRecord(array) {[
+  {firstName => array[0], familyName => array[1], title => array[2], payPerHour => array[3], timeInEvents => [], timeOutEvents => []} 
+]}
